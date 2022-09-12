8539e665def881ef49834f0f1fa8e5b8db599ea2
JESS_WYATT <jess.wyatt@gmail.com>
2022-09-12 20:58:02 -0230

    Updates

---
+++ precache-manifest.be3752da626a28512baaa317e03e61df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ebec4272af7ea2dad0da6c8590692d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7374aa37f5f1a7b0587",
+    "url": "/static/css/main.0f3029b9.chunk.css"
+  },
+  {
+    "revision": "aaa2a7cb7bd6740df560",
+    "url": "/static/js/2.2a580e01.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.2a580e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7374aa37f5f1a7b0587",
+    "url": "/static/js/main.f5953669.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f5953669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80c8820d192aa34f2ea",
+    "url": "/static/js/runtime-main.e7e753f1.js"
+  }
+]);
